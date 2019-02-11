e0cfe787dd16beb24e0cb8f10cc8bd3ba77aec8e
Eiji Hachiya <hachiyae@gmail.com>
2019-02-11 18:27:34 +0900

    implement api mock

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './reducers';
-import { EffectsModule } from '@ngrx/effects';
 import { AppEffects } from './app.effects';
+import { InMemoryUserService } from './mocks/in-memory-user.service';
+import { reducers, metaReducers } from './reducers';
 
 @NgModule({
   declarations: [
@@ -15,6 +18,8 @@ import { AppEffects } from './app.effects';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryUserService, { delay: 500 }),
     StoreModule.forRoot(reducers, { metaReducers }),
     EffectsModule.forRoot([AppEffects])
   ],
--- src/app/mocks/in-memory-user.service.ts
+++ src/app/mocks/in-memory-user.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
-export class InMemoryUserService {
+export class InMemoryUserService implements InMemoryDbService {
+  constructor() {
+  }
 
-  constructor() { }
+  private static getUsers(): Array<User> {
+    return [
+      { id: 1, name: 'mike' },
+      { id: 2, name: 'bob' },
+      { id: 3, name: 'taro' },
+      { id: 3, name: 'joe' },
+    ];
+  }
+
+  createDb() {
+    return {
+      users: InMemoryUserService.getUsers()
+    };
+  }
+
 }
