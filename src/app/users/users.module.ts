ebb4c7268fc280a60b404b7e352498bd9945cb86
Eiji Hachiya <hachiyae@gmail.com>
2019-02-11 18:36:04 +0900

    ng g component users/user-list -m

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [UserListComponent],
   imports: [
     CommonModule,
     UsersRoutingModule
