b58edc7effe3bfabf48a55380b66a06dfa8fc08e
Eiji Hachiya <hachiyae@gmail.com>
2019-02-11 18:10:12 +0900

    add debug

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -15,5 +15,11 @@ export const reducers: ActionReducerMap<State> = {
 
 };
 
+export function debug(reducer: ActionReducer<any>): ActionReducer<any> {
+  return (state, action) => {
+    console.log('state', state, 'action', action);
+    return reducer(state, action);
+  };
+}
 
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+export const metaReducers: MetaReducer<State>[] = !environment.production ? [debug] : [];
